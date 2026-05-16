0fa4b374418cc05be4f8323b5438474abc9524ee
Mamdouh_Nafee <mamdouhdido5@gmail.com>
2026-05-16 21:04:19 +0300

    updated filters, to accommodate for FIR and cheby1,2 for IIR

--- Part 2/BPF.m
+++ Part 2/BPF.m
@@ -1,13 +1,35 @@
-function [b,a] = BPF(lowerBound,upperBound,Fs,order)
-%LPF Summary of this function goes here
-%   Detailed explanation goes here
-    wp = [lowerBound/(Fs/2) upperBound/(Fs/2)];
-    ws = [ max(lowerBound-100,1) upperBound+100 ] /(Fs/2);
-    ws(2) = min(ws(2), 0.99); 
-    
-    [N, wn] = buttord(wp,ws,0.5,20);
-    N = min(N,floor(order/2));
-    [b,a] = butter(N,wn);
+function [b, a] = BPF(lowerBound, upperBound, Fs, order, filterType, windowType, iirType)
+    wp = [lowerBound   upperBound] / (Fs/2);
+    wp(1) = max(wp(1), 0.01);
+    wp(2) = min(wp(2), 0.99);
 
+    if strcmp(filterType, 'IIR')
+        ws = [max(lowerBound-100, 1)  (upperBound+100)] / (Fs/2);
+        ws(1) = max(ws(1), 0.01);
+        ws(2) = min(ws(2), 0.99);
+
+        switch iirType
+            case 'Butterworth'
+                [N, wn_out] = buttord(wp, ws, 0.5, 20);
+                N = min(N, floor(order/2));
+                [b, a] = butter(N, wn_out);
+
+            case 'Chebyshev I'
+                [N, wn_out] = cheb1ord(wp, ws, 0.5, 20);
+                N = min(N, floor(order/2));
+                [b, a] = cheby1(N, 0.5, wn_out);
+
+            case 'Chebyshev II'
+                [N, wn_out] = cheb2ord(wp, ws, 0.5, 20);
+                N = min(N, floor(order/2));
+                [b, a] = cheby2(N, 20, wn_out);
+        end
+    else  % FIR
+        if mod(order, 2) ~= 0
+            order = order + 1;
+        end
+        win = getWindow(order, windowType);
+        b = fir1(order, wp, 'bandpass', win);
+        a = 1;
+    end
 end
-
--- Part 2/LPF.m
+++ Part 2/LPF.m
@@ -1,12 +1,29 @@
-function [b,a] = LPF(lowerBound,upperBound,Fs,order)
-%LPF Summary of this function goes here
-%   Detailed explanation goes here
-    wp = upperBound / (Fs / 2);
-    ws = (upperBound + 100) / (Fs / 2);
+function [b, a] = LPF(lowerBound, upperBound, Fs, order, filterType, windowType, iirType)
+    wn = upperBound / (Fs/2);
+    wn = min(wn, 0.99);
 
-    [N, wn] = buttord(wp,ws,0.5,20);
-    N = min(N,order);
-    [b,a] = butter(N,wn,'low');
+    if strcmp(filterType, 'IIR')
+        ws = min((upperBound + 100) / (Fs/2), 0.99);
 
+        switch iirType
+            case 'Butterworth'
+                [N, wn_out] = buttord(wn, ws, 0.5, 20);
+                N = min(N, order);
+                [b, a] = butter(N, wn_out, 'low');
+
+            case 'Chebyshev I'
+                [N, wn_out] = cheb1ord(wn, ws, 0.5, 20);
+                N = min(N, order);
+                [b, a] = cheby1(N, 0.5, wn_out, 'low');
+
+            case 'Chebyshev II'
+                [N, wn_out] = cheb2ord(wn, ws, 0.5, 20);
+                N = min(N, order);
+                [b, a] = cheby2(N, 20, wn_out, 'low');
+        end
+    else
+        win = getWindow(order, windowType);
+        b = fir1(order, wn, 'low', win);
+        a = 1;
+    end
 end
-
